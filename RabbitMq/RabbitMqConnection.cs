32fe30423b9877946d11d0e89802d34e39b73dd1
sachem1 <jieqqq678@163.com>
2019-07-08 07:46:22 +0800

    rabbitmq test

---
+++ RabbitMq/RabbitMqConnection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace RabbitMq
+{
+    public class RabbitMqConnection
+    {
+        public IConnection GetConnection()
+        {
+            var factory = new ConnectionFactory();
+            factory.UserName = "admin";
+            factory.Password = "admin123";
+            factory.HostName = "59.110.216.148";
+
+            IConnection conn = factory.CreateConnection();
+            return conn;
+        }
+    }
+}
