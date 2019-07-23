e4c23b6abebafd05902aada684a41993becc3856
sachem1 <jieqqq678@163.com>
2019-07-24 06:45:20 +0800

    add scheduler

---
+++ Jiesen.Common/LogManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Jiesen.Common
+{
+    public class LogManager
+    {
+        public static void AppendLogFile(string message)
+        {
+            var fileUrl = AppDomain.CurrentDomain.BaseDirectory + "Log//" + DateTime.Now.ToString("yyyy-MM-dd HH") +
+                          ".txt";
+
+            using (FileStream fs = new FileStream(fileUrl, FileMode.OpenOrCreate))
+            {
+                message = message + $"当前时间是:{DateTime.Now:yyyy-MM-dd HH:mm:ss}\r\n";
+                var bytes = Encoding.UTF8.GetBytes(message);
+                fs.Position = fs.Length;
+                fs.Write(bytes, 0, bytes.Length);
+                fs.Flush(true);
+            }
+        }
+    }
+}
