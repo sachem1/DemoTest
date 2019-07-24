6ecfcdc9c8103451752b152149e4258ac4529c88
sachem1 <jieqqq678@163.com>
2019-07-25 07:06:40 +0800

    add dapperpoco

---
+++ Jiesen.ConsoleApp/MysqlDbcontext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.SqlClient;
+using Mondol.DapperPoco;
+using Mondol.DapperPoco.Adapters;
+
+namespace Jiesen.ConsoleApp
+{
+    public class MysqlDbcontext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseConnectionString("连接字符串");
+            //使用SQL Server数据库
+            optionsBuilder.UseSqlAdapter(new SqlServerAdapter(SqlClientFactory.Instance));
+        }
+    }
+}
---
+++ Jiesen.ConsoleApp/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace Jiesen.ConsoleApp
+{
+    public class Student
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ClassName { get; set; }
+    }
+}
